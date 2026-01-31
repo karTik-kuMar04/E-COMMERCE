274045f49986355fceca15e42685bd7bf38b510b
karTik-kuMar04 <mysoulisinfinity1@gmail.com>
2026-01-31 17:06:26 +0530

    Fix: db change part 2

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -3,9 +3,14 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG error", err);
 });
 
 export default pool;
